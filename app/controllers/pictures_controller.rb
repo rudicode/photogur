9f4df4fec68d043c86340353b84d340aa213e447
Rudi Luzar <rudi@rudicode.com>
2014-11-04 16:39:09 -0500

    Added feature to submit a new picture using a form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -11,6 +11,14 @@ def index
 
   end
 
+  def new
+    
+  end
+
+  def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
+
 end
 
 def picture_array
