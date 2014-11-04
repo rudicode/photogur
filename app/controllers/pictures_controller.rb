aa6a998b4b9b6119d686e78196f5ddb702b97800
Rudi Luzar <rudi@rudicode.com>
2014-11-04 17:01:05 -0500

    Storing the picture data in database, updated index and show to reflect these changes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,12 @@
 class PicturesController < ApplicationController
 
 def index
-  @pictures = picture_array
+  @pictures = Picture.all
   end
 
   def show
     
-  @pictures = picture_array
-  @picture = @pictures[params[:id].to_i]
+  @picture = Picture.find(params[:id])
 
   end
 
@@ -21,22 +20,3 @@ def index
 
 end
 
-def picture_array
-  [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-      }
-    ]
-end
