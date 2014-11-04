03d628bcfbb3276fde059c92c0d3e33f16a2da52
Rudi Luzar <rudi@rudicode.com>
2014-11-04 18:02:50 -0500

    Added the feature to delete a Picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,13 @@
 class PicturesController < ApplicationController
+  
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def index
     @pictures = Picture.all
   end
 
   def show
-    
-    @picture = Picture.find(params[:id])
-
+    # @picture = Picture.find(params[:id])
   end
 
   def new
@@ -25,11 +25,11 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
   end
 
   def update
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
     
     if @picture.update_attributes(picture_params)
       redirect_to picture_url(@picture)
@@ -38,12 +38,23 @@ class PicturesController < ApplicationController
     end
   end
 
+  def destroy
+    # @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+
+  end
+
 private
 
     def picture_params
 
       params.require(:picture).permit(:artist, :title, :url)
 
+    end
+
+    def set_picture
+      @picture = Picture.find(params[:id])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
   post 'pictures'     => 'pictures#create'
@@ -10,12 +9,17 @@ Rails.application.routes.draw do
   get   'pictures/:id/edit' => 'pictures#edit', as: "edit_picture"
   patch 'pictures/:id'      => 'pictures#update'
 
+  delete 'pictures/:id'     => 'pictures#destroy', as: "delete_picture"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'pictures#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
